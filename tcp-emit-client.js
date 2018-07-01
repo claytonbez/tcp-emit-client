4f64fbca46d7cfcdb4fbf9075a5ca723c16c0c1a
Clayton Bezuidenhout <bezclayton@gmail.com>
2018-07-01 16:19:29 +0200

    Changed base send format to JSON

--- tcp-emit-client.js
+++ tcp-emit-client.js
@@ -1,152 +1,166 @@
+var net = require('net');
 var util = require('util');
 var fs = require('fs');
 var EventEmitter = require('events').EventEmitter;
-var SocketEmitter = require('events').EventEmitter;
-var net = require('net');
-var socket;
-var brStr = "~!";
-var initSocket = function(addr,port){
+var { Writable } = require('stream');
+var { Readable } = require('stream');
+var ec = String.fromCharCode(13);
+
+var file = function () { }
+util.inherits(file, EventEmitter);
+var stream = function (options) {
+    if (!(this instanceof stream))
+        return new stream(options);
+    Writable.call(this, options);
+}
+util.inherits(stream, EventEmitter);
+
+var mainSocket, socket;
+
+var initSocket = function (addr, port) {
     var self = this;
     self.oldEmit = self.emit; //going out
-    self.emit = function (evt, data, data2, data3) { // .on events inside initSocket
-        self.oldEmit('msg', evt, data, data2, data3);
-    }
-    self.sendFile = function (file) {
-        var rs = fs.createReadStream(file);
-        socket.write(`^o${brStr}${file}`);
-        rs.on('data', function (buffer) {
-            var b = buffer.toString('utf8');
-            socket.write(`^d${brStr}${file}${brStr}${b}`);
-        });
-        rs.on('close', function () {
-            console.log('file closed');
-            setTimeout(function () {
-                socket.write(`^c${brStr}${file}`);
-            }, 500);
-        });
+    self.emit = function (evt, data) { // incoming from implementation .on events inside initSocket
+        self.oldEmit('msg', evt, data);
     }
 
-    var socket = new net.Socket();
-    socket = net.connect(port, addr, function () {
-        self.oldEmit('connect');
-    });
-    socket.on('data', function (buffer) {
+    socket = new net.Socket();
+    socket = net.connect(port, addr, function () { self.oldEmit('connect'); });
+
+    socket.on('data', function (buffer) { // forward buffer to mainProcess
         self.oldEmit('data', buffer);
     });
-    socket.on('close', function () {
+    socket.on('close', function () {// forward close to mainProcess
         self.oldEmit('close');
     });
-    socket.on('error', function (err) {
-        self.oldEmit('err,', err);
-    });
-    self.on('msg',function(evt,data,data2,data3){
-        var strConst = `${evt}${brStr}`
-        try {
-            data = JSON.stringify(data);
-        } catch (e) { }
-        strConst += `${data}`;
-        if (data2) {
-            try {
-                data2 = JSON.stringify(data2);
-            } catch (e) { }
-            strConst += `${brStr}${data2}`;
-        }
-        if (data3) {
-            try {
-                data3 = JSON.stringify(data3);
-            } catch (e) { }
-            strConst += `${brStr}${data3}`;
-        }
-        socket.write(strConst);
+    socket.on('error', function (err) { // forward error to mainProcess
+        self.oldEmit('error', err);
     });
 
+    self.on('msg', function (event, data) { // listen for msg events on initSocket and write json protocol to the raw socket
+        var obj = { e: event, d: data };
+        var strConst = JSON.stringify(obj);
+        socket.write(strConst + ec);
+    });
+    self.sendFile = function (fn, fp, callback) {
+        fs.stat(fp, function (err, stats) {
+            if (!err) {
+                var obj = { f: fn, fs: stats.size, s: 1 };
+                socket.write(JSON.stringify(obj) + ec);
+                var rs = fs.createReadStream(fp);
+                rs.on('data', function (chunk) {
+                    socket.write(JSON.stringify({ f: obj.f, b: chunk }) + ec);
+                });
+                rs.on('error', function (err) {
+                    callback(false, err);
+                });
+                rs.on('end', function () {
+                    (function () {
+                        var o = obj;
+                        setTimeout(function () {
+                            socket.write(JSON.stringify({ f: o.f, c: 1 }) + ec);
+                            callback(true);
+                        }, 500);
+                    })();
+                });
+            } else {
+                socket.oldEmit('error', err);
+            }
+        });
+    }
 }
-util.inherits(initSocket, SocketEmitter);
-var fileHolders = []; 
-var file = function (fn) { 
-    this.filename = fn;
-}
-util.inherits(file, EventEmitter);
-var main = function(addr,port){
+util.inherits(initSocket, EventEmitter);
+var setDelete = 0;
+var rawbuffer = '';
+var fileHolder;
+var main = function (addr, port) {
     var self = this;
     self.oldEmit = self.emit;
-    self.emit = function (evt, data, data2, data3) { 
-        mainSocket.emit(evt, data, data2, data3);
+    self.emit = function (evt, data) { // forward main process event to initSocket
+        mainSocket.emit(evt, data, );
     }
-    self.setBreakStr = function (str) {
-        brStr = str;
-    };
-    var mainSocket;
-    var mainSocketFunc = function(addr,port){
-        if(mainSocket){ delete(mainSocket);}
+    self.destroy = function () {
+        self.oldEmit('destroy');
+        setDelete = 1;
+        socket.destroy();
+        delete (mainSocket);
+    }
+    self.sendFile = function (fn, fp, callback) {
+        mainSocket.sendFile(fn, fp, function (done, err) {
+            if (done) {
+                callback(true);
+            }
+            else {
+                callback(false, err);
+            }
+
+        });
+    }
+    var mainSocketFunc = function (addr, port) {
+        if (mainSocket) { delete (mainSocket); }
         mainSocket = new initSocket(addr, port);
         mainSocket.on('connect', function () {
+
             self.oldEmit('connect');
         });
         mainSocket.on('data', function (buffer) {
-            var data = buffer.toString();
-            self.oldEmit('data', data);
-            var d = data.split(brStr);
-            switch(d[0]){
-                case "^o":
-                    console.log('openfile')
-                    var fn = d[1]
-                    fileHolders[fn] = new file(fn);
-                    self.oldEmit('file', fileHolders[fn]);
-                    break;
-                case "^d":
-                    var fn = d[1]
-                    var buffer = Buffer.from(d[2],'utf8')
-                    fileHolders[fn].emit('data', buffer);
-                    break;
-                case "^c":
-                    var fn = d[1]
-                    fileHolders[fn].emit('end');
-                    var i = fileHolders.indexOf(fileHolders[fn]);
-                    fileHolders.splice(i,1);
-                    break;
-                default:
-                    if (d.length == 2) {
-                        var evt = d[0];
-                        var sendData = d[1];
-                        try { sendData = JSON.parse(sendData); }
-                        catch (e) { }
-                        self.oldEmit(evt, sendData);
+            var dta = buffer.toString();
+            var lc = dta[dta.length - 1];
+            rawbuffer = rawbuffer + dta;
+            if (lc == String.fromCharCode(13)) {
+                var data = rawbuffer;
+                rawbuffer = '';
+                data = data.substr(0, data.length - 1);
+                try {
+                    var obj = JSON.parse(data);
+                    if (obj.e) {
+                        var evt = obj.e;
+                        var sd = obj.d;
+                        self.oldEmit(evt, sd);
                     }
-                    if (d.length == 3) {
-                        var evt = d[0];
-                        var sendData = d[1], sendData2 = d[2];
-                        try { sendData = JSON.parse(sendData); } catch (e) { }
-                        try { sendData2 = JSON.parse(sendData2); } catch (e) { }
-                        self.oldEmit(evt, sendData, sendData2);
+                    else {
+                        if (obj.f) {
+                            if (obj.s) {
+                                fileHolder = new file(obj.f);
+                                fileHolder.filename = obj.f;
+                                fileHolder.size = obj.fs;
+                                self.oldEmit('file', fileHolder);
+                            }
+                            if (obj.b) {
+                                var buffer = Buffer.from(obj.b, 'utf8')
+                                fileHolder.emit('data', buffer);
+                            }
+                            if (obj.c) {
+                                fileHolder.emit('end');
+                                fileholder = null;
+                            }
+                        }
                     }
-                    if (d.length == 4) {
-                        var evt = d[0];
-                        var sendData = d[1], sendData2 = d[2], sendData3 = d[3];
-                        try { sendData = JSON.parse(sendData); } catch (e) { }
-                        try { sendData2 = JSON.parse(sendData2); } catch (e) { }
-                        try { sendData3 = JSON.parse(sendData3); } catch (e) { }
-                        self.oldEmit(evt, sendData, sendData2, sendData3);
-                    }
-
+                } catch (err) {
+                    console.log(err)
+                    self.oldEmit('error', err);
+                }
             }
-             });
+        });
         mainSocket.on('close', function () {
             self.oldEmit('disconnect');
-            (function(){
-                var ad = addr;
-                var pr = port;
-                setTimeout(function(){
-                    self.oldEmit('reconnect')
-                    mainSocketFunc(addr, port);
-                },1000);                    
-            })();
+            if (!setDelete) {
+                (function () {
+                    var ad = addr;
+                    var pr = port;
+                    setTimeout(function () {
+                        self.oldEmit('reconnect')
+                        mainSocketFunc(addr, port);
+                    }, 3000);
+                })();
+            }
+
         });
         mainSocket.on('error', function (err) {
             self.oldEmit('err', err);
         });
     }
-    mainSocketFunc(addr,port);
+    mainSocketFunc(addr, port);
 }
 util.inherits(main, EventEmitter);
 module.exports = main; 
